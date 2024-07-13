7a9ecdc6371868bab0b70807854574b036e50a30
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-07-13 06:31:52 -0400

    Revert "Updated next.js for export"
    This reverts commit 16e1f4bffebdd2b8c829f7122c15a455a09369a3.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// next.config.js
-module.exports = {
-    reactStrictMode: true,
-    images: {
-      unoptimized: true, // This is required for static exports
-    },
-    output: 'export',
-  };
-  
 export default nextConfig;
