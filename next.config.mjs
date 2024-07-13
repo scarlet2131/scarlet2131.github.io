16e1f4bffebdd2b8c829f7122c15a455a09369a3
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-07-13 06:25:57 -0400

    Updated next.js for export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    images: {
+      unoptimized: true, // This is required for static exports
+    },
+    output: 'export',
+  };
+  
 export default nextConfig;
