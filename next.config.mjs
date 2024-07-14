402a40cdd5d41256c843476cea8e507496b7bbfa
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-07-14 15:49:28 -0400

    fixing static content display

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
-      },
+    },
+    basePath: '/Internship_Portfolio',
+    assetPrefix: '/Internship_Portfolio',
 
 };
 export default nextConfig;
