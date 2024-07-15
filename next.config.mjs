3af8662b6ab571b19b846b267b4e113a2d728f2f
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-07-15 17:23:23 -0400

    updating the nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    // images: {
-    //     unoptimized: true,
-    // },
-    // basePath: '/scarlet2131.github.io',
-    // assetPrefix: '/scarlet2131.github.io',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/scarlet2131.github.io',
+    assetPrefix: '/scarlet2131.github.io',
 };
 export default nextConfig;
